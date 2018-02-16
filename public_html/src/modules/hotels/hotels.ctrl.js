e0eee0dec7e6e21862270ce6c0688f60f23508db
Diego F. Ruiz S <yego23@gmail.com>
2018-02-16 13:10:01 -0500

    filters frontend

--- public_html/src/modules/hotels/hotels.ctrl.js
+++ public_html/src/modules/hotels/hotels.ctrl.js
@@ -4,14 +4,52 @@
 	mod.controller('hotelCtrl', ['$scope', '$http', 'hotelContext',
 		function ($scope, $http, hotelContext) {
 
-			$http.get(hotelContext).then(function(response){
-				$scope.hotels = response.data;
-			});
+			$scope.show = false;
+
+			$scope.getHotels = function(){
+				$http.get(hotelContext).then(function(response){
+					$scope.hotels = response.data;
+				});	
+			}
+
+			$scope.searchHotel = function(search){
+
+				$http.get(hotelContext, {params: {name: search}}).then(function(response){
+					$scope.hotels = response.data;
+					$scope.show = false;
+					$scope.validateResults($scope.hotels);
+				});
+
+
+				if(search==''){
+					$scope.getHotels()
+				}
+			}
+
+			$scope.searchHotelWithStars = function(stars){
+
+				$http.get(hotelContext, {params: {stars: stars}}).then(function(response){
+					$scope.hotels = response.data;
+					$scope.show = false;
+					$scope.validateResults($scope.hotels);
+					
+				});
+
+			}
+
+			$scope.validateResults = function(hotels){
+				if(hotels.length===0){
+					$scope.show = true;
+					$scope.message = "Lo sentimos no hemos encontrado resultados...";
+				}
+			}
+
 
 			$scope.range = function(n) {
 				return new Array(n);
 			};
 
+			$scope.getHotels();
 
 		}
 		]);
