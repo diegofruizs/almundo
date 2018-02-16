f2e7a4c7119678448310842c08c5ef7b1a6217ae
Diego F. Ruiz S <yego23@gmail.com>
2018-02-16 10:30:45 -0500

    filters in api GET hotels

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# example app with nodejs as backend and angularjs as frontend
+#Example app with nodejs as backend and angularjs as frontend
+
+Para ejecutar localmente
+* `npm install` - para dependencias para backend
+* `bower install` - para las dependencias del frontend
+* instalar mongodb
+* iniciar mongodb con la instrucción `mongod`
+* iniciar la aplicación `npm start`
+* verificar en `localhost:3000`
 
 @diegofruizs
--- controllers/hotel.js
+++ controllers/hotel.js
@@ -8,51 +8,83 @@ function getHotel(req, res){
 
 	Hotel.findById(hotelId,(err,hotel) => {
 		if(err) return res.status(500).send({message: 'Error al realizar la petición'})
-	    if(!hotel) return res.status(404).send({message: 'El hotel con ese id no existe'})
+			if(!hotel) return res.status(404).send({message: 'El hotel con ese id no existe'})
 
-	    res.status(200).send(hotel)	
-	})
+				res.status(200).send(hotel)	
+		})
 
 }
 
 function getHotels(req,res){
 
-Hotel.find({},(err,hotels)=>{
+	if(!isEmpty(req.query)){
+
+		if(req.query.name){
+			var name = req.query.name;
+
+			Hotel.find({'name' : new RegExp(name, 'i')}, function(err, hotels){
+				if(err) return res.status(500).send({message: 'Error al realizar el filtro de los hoteles por nombre'})
+				
+				if(!hotels) return res.status(404).send({message: 'No hay resultados para su búsqueda por nombre'})
+				
+				res.status(200).send(hotels)	
+
+				});	
+		}
+		if(req.query.stars){
+			var stars = req.query.stars;
+
+			Hotel.find({'stars' : stars}, function(err, hotels){
+				if(err) return res.status(500).send({message: 'Error al realizar el filtro de los hoteles por estrellas'})
+				
+				if(!hotels) return res.status(404).send({message: 'No hay resultados para su búsqueda por estrellas'})
+				
+				res.status(200).send(hotels)	
+
+				});	
+
+		}
+
+	}else{
+
+	Hotel.find({},(err,hotels)=>{
 		if(err) return res.status(500).send({message: 'Error al realizar la petición de consultar los hoteles'})
-	    if(!hotels) return res.status(404).send({message: 'No hay hoteles disponibles '})
-	   
-	    res.status(200).send(hotels)	
+		
+		if(!hotels) return res.status(404).send({message: 'No hay hoteles disponibles '})
+		
+		res.status(200).send(hotels)	
 
 
-	})
+		})
+	}
 }
 
 function saveHotel(req,res){
 	console.log('POST /api/hotel')
 	for(var key in req.body) {
- 	 if(req.body.hasOwnProperty(key)){
+		if(req.body.hasOwnProperty(key)){
 
- 	 	console.log(req.body[key]);
-	
+			console.log(req.body[key]);
 
-  		}
+
+		}
 	}
 
 	let hotel = new Hotel()
 	hotel.id = req.body.id
 	hotel.name= req.body.name
-    hotel.stars = req.body.stars
-    hotel.price = req.body.price
-    hotel.image= req.body.image
-    hotel.amenities = req.body.amenities
+	hotel.stars = req.body.stars
+	hotel.price = req.body.price
+	hotel.image= req.body.image
+	hotel.amenities = req.body.amenities
 
 
-    hotel.save((err, hotelStored) =>{
-    	if(err){
-    		res.status(500).send({message: `Error guardando... ${err}`})
-    	}
-       res.status(200).send({hotel: hotelStored})	
-    })
+	hotel.save((err, hotelStored) =>{
+		if(err){
+			res.status(500).send({message: `Error guardando... ${err}`})
+		}
+		res.status(200).send({hotel: hotelStored})	
+	})
 
 
 
@@ -68,7 +100,7 @@ function updateHotel(req, res){
 	Hotel.findByIdAndUpdate(hotelId, {$set:update}, (err,hotelUpdated) =>{
 		if(err) res.status(500).send({message: `Error actualizando el hotel... ${err}`})
 
-		res.status(200).send({hotel: hotelUpdated})
+			res.status(200).send({hotel: hotelUpdated})
 
 	})	
 
@@ -78,18 +110,27 @@ function deleteHotel(req, res){
 
 	let hotelId = req.params.hotelId
 
-Hotel.findById(hotelId, (err,hotel) =>{
+	Hotel.findById(hotelId, (err,hotel) =>{
 
-	if(err) res.status(500).send({message: `Error borrando el hotel... ${err}`})
-	hotel.remove(err=>{
 		if(err) res.status(500).send({message: `Error borrando el hotel... ${err}`})
-		res.status(200).send({message: 'Hotel borrado...'})	
-	})	
+			hotel.remove(err=>{
+				if(err) res.status(500).send({message: `Error borrando el hotel... ${err}`})
+					res.status(200).send({message: 'Hotel borrado...'})	
+			})	
 
 
-})
+	})
 
 
+}
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+
+    return true;
 }
 
 module.exports = {
